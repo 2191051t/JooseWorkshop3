4378e0767e26965ce50998fadee3f2fe0ef92d17
Greg Tucker <2191051t@student.gla.ac.uk>
2017-03-13 15:07:13 +0000

    Tried to implement Task 1,2,3

--- src/main/java/gla/cs/joose/coursework/invmgt/model/InvmgtClient.java
+++ src/main/java/gla/cs/joose/coursework/invmgt/model/InvmgtClient.java
@@ -2,28 +2,33 @@ package gla.cs.joose.coursework.invmgt.model;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 
 public class InvmgtClient {
 	private static Client client;
 	private static WebTarget baseTarget;
 	private static WebTarget itemsTarget;
 	private static WebTarget itemTarget;
-	
+
 	/**
 	 * Constructor to initialise a REST client and web targets (resource URIs)
 	 * DO NOT MODIFY
 	 */
-	public InvmgtClient(){
+	public InvmgtClient() {
 		client = ClientBuilder.newClient();
-		
+
 		baseTarget = client.target("http://localhost:8080/invmgtapi/webapi/invapi");
 		itemsTarget = baseTarget.path("items");
 		itemTarget = baseTarget.path("items").path("{itemid}");
 	}
-	
+
 	/**
-	 * This method makes a REST API call to invmgtapi service for an update to an item in the inventory.
+	 * This method makes a REST API call to invmgtapi service for an update to
+	 * an item in the inventory.
 	 * 
 	 * @param updateitemid
 	 * @param newBarcode
@@ -32,60 +37,68 @@ public class InvmgtClient {
 	 * @param newQty
 	 * @param newSupplier
 	 * @param newDesc
-	 * @return returns the updated item if updateRequest is successful or the error status code if unsuccessful
+	 * @return returns the updated item if updateRequest is successful or the
+	 *         error status code if unsuccessful
 	 */
-	public Object updateRequest(long updateitemid,  long newBarcode, 
-							 String newItemName, String newItemType_s,
-							 int newQty, String newSupplier, String newDesc){
-		//Task 1
-		
-		return null;	
+	public Object updateRequest(long updateitemid, long newBarcode, String newItemName, String newItemType_s,
+			int newQty, String newSupplier, String newDesc) {
+		Item uitem = new Item(newBarcode, newItemName, ItemType.valueOf(newItemType_s.toUpperCase()), newQty, newSupplier, newDesc);
+		Builder builder = itemTarget.resolveTemplate("itemid", updateitemid).request();
+		Response putResponse = builder.put(Entity.json(uitem));
+		if (putResponse.getStatus() != 200) {
+			return putResponse.getStatus();
+		}
+		Object item = putResponse.readEntity(Item.class);
+		return item;
 	}
-	
+
 	/**
-	 * This method makes a REST API call to invmgtapi service to delete an item from the inventory.
+	 * This method makes a REST API call to invmgtapi service to delete an item
+	 * from the inventory.
 	 * 
 	 * @param itemid
 	 * @return returns a status code indicating the outcome of deleteRequest
 	 */
-	public int deleteRequest(long itemid){
-		//Task 2
-		
-		return -1;
+	public int deleteRequest(long itemid) {
+		Builder builder = itemTarget.resolveTemplate("itemid", itemid).request();
+		Response deleteResponse = builder.delete();
+		return deleteResponse.getStatus();
 	}
-	
+
 	/**
-	 * This method makes a REST API call to invmgtapi service to retrieve items that matches a 
-	 * specific search pattern from the inventory.
+	 * This method makes a REST API call to invmgtapi service to retrieve items
+	 * that matches a specific search pattern from the inventory.
 	 * 
 	 * @param searchbydesc
 	 * @param pattern
 	 * @param limit
 	 * @return returns a list of items that matches the searchRequest parameters
 	 */
-	public Item[] searchRequest(String searchbydesc, String pattern, int limit){
-		
-		//Task 3
+	public Item[] searchRequest(String searchbydesc, String pattern, int limit) {
 		
 		return null;
-		
+
 	}
-	
+
 	/**
-	 * This method makes a REST API call to invmgtapi service to add an item to the inventory
-	 *  
+	 * This method makes a REST API call to invmgtapi service to add an item to
+	 * the inventory
+	 * 
 	 * @param barcode
 	 * @param itemName
 	 * @param itemType_s
 	 * @param qty
 	 * @param supplier
 	 * @param desc
-	 * @return - returns a REST response status code indicating the outcome of addItemRequest
+	 * @return - returns a REST response status code indicating the outcome of
+	 *         addItemRequest
 	 */
-	public int addItemRequest(long barcode, String itemName, String itemType_s, int qty, String supplier, String desc){
-				
-		//Task 4
+	public int addItemRequest(long barcode, String itemName, String itemType_s, int qty, String supplier, String desc) {
 		
-		return -1;
+		Item newItem = new Item(barcode, itemName, ItemType.valueOf(itemType_s.toUpperCase()), qty, supplier, desc);			
+		Builder builder = itemsTarget.request();
+		Response postResponse = builder.post(Entity.json(newItem));
+		System.out.println(postResponse.getStatus());
+		return postResponse.getStatus();
 	}
 }
